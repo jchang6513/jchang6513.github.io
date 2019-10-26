c4d23035f6d86ae1e809ba2d7b2f0a10a39afadc
Yi-Wei Chang <jchang6513@gmail.com>
2019-10-26 12:28:03 +0800

    Add keyboard-tester

---
+++ keyboard-tester/precache-manifest.7d33412b86127c945b6a7e1f83052aef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264fd36c94cab894e1b00e2a9753c36c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a5f627c12f8e268063",
+    "url": "/static/css/main.06d60612.chunk.css"
+  },
+  {
+    "revision": "448c5cd430a1d722f77c",
+    "url": "/static/js/2.4a313aef.chunk.js"
+  },
+  {
+    "revision": "18a5f627c12f8e268063",
+    "url": "/static/js/main.afa3f54d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
