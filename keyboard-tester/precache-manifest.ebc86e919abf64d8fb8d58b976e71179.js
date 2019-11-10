edfd8eb72c4c40813c85fe1f7649caf8ab09cba5
Yi-Wei Chang <jchang6513@gmail.com>
2019-11-10 16:13:13 +0800

    Update keyboard-tester

---
+++ keyboard-tester/precache-manifest.ebc86e919abf64d8fb8d58b976e71179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0448835f3c59ce942fb7bf71e57ffd",
+    "url": "./keyboard-tester/../keyboard-tester.html"
+  },
+  {
+    "revision": "1c84e7a9da988efb7bc1",
+    "url": "./keyboard-tester/static/css/main.08de1f98.chunk.css"
+  },
+  {
+    "revision": "448c5cd430a1d722f77c",
+    "url": "./keyboard-tester/static/js/2.4a313aef.chunk.js"
+  },
+  {
+    "revision": "1c84e7a9da988efb7bc1",
+    "url": "./keyboard-tester/static/js/main.ceaaad59.chunk.js"
+  },
+  {
+    "revision": "7542f8f6eea9ccbc407d",
+    "url": "./keyboard-tester/static/js/runtime~main.29f364db.js"
+  }
+]);
