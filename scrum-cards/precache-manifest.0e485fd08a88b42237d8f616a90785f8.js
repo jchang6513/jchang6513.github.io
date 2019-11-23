8f986b169ac83bcc7dd5b674ab57e839196edb49
Yi-Wei Chang <jchang6513@gmail.com>
2019-11-23 16:23:08 +0800

    Add scrum-cards

---
+++ scrum-cards/precache-manifest.0e485fd08a88b42237d8f616a90785f8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a757466392dbaf7f33daccd88933c4",
+    "url": "./scrum-cards/../scrum-cards.html"
+  },
+  {
+    "revision": "83c74f6986f573143834",
+    "url": "./scrum-cards/static/css/main.fea1f3a8.chunk.css"
+  },
+  {
+    "revision": "6d5ebc1c9a019f95fddd",
+    "url": "./scrum-cards/static/js/2.92d48c4e.chunk.js"
+  },
+  {
+    "revision": "83c74f6986f573143834",
+    "url": "./scrum-cards/static/js/main.9f46abaa.chunk.js"
+  },
+  {
+    "revision": "802333ebf77e527f0687",
+    "url": "./scrum-cards/static/js/runtime~main.4c756283.js"
+  },
+  {
+    "revision": "9cd9de53e26d23b6fcb7d4b972f95ef7",
+    "url": "./scrum-cards/static/media/Cover - option 2.9cd9de53.png"
+  },
+  {
+    "revision": "e9ab56291cbac7436ec0ad0e02d1df4b",
+    "url": "./scrum-cards/static/media/planning poker-03.e9ab5629.png"
+  },
+  {
+    "revision": "98d6a8aeec2fbcba94bac0dc7c2a8f7e",
+    "url": "./scrum-cards/static/media/planning poker_An arm and a leg.98d6a8ae.png"
+  },
+  {
+    "revision": "ff325f82397728e998340d64fd775562",
+    "url": "./scrum-cards/static/media/planning poker_Don't put all .ff325f82.png"
+  },
+  {
+    "revision": "ff0919ae6b095a75bd0dfbbfbedebcc7",
+    "url": "./scrum-cards/static/media/planning poker_Eat a brownie.ff0919ae.png"
+  },
+  {
+    "revision": "457cfbe61892da22757b7865968b8e7c",
+    "url": "./scrum-cards/static/media/planning poker_Here be dragons.457cfbe6.png"
+  },
+  {
+    "revision": "ce68cf66981ad5ab49e786026cb1fc62",
+    "url": "./scrum-cards/static/media/planning poker_It ain't rocket science.ce68cf66.png"
+  },
+  {
+    "revision": "fa1d10e7f87f2cda63c552426b75375a",
+    "url": "./scrum-cards/static/media/planning poker_Low hanging fruit.fa1d10e7.png"
+  },
+  {
+    "revision": "a04b591d4d96c5e7480ae3a9b16d0e88",
+    "url": "./scrum-cards/static/media/planning poker_Meterse en un berenjenal.a04b591d.png"
+  },
+  {
+    "revision": "c67fcd80821e29999960ce0b7338fb9a",
+    "url": "./scrum-cards/static/media/planning poker_Monster task.c67fcd80.png"
+  },
+  {
+    "revision": "fd9db3736cfc3a2bd85fed420c3f8894",
+    "url": "./scrum-cards/static/media/planning poker_Piece of cake.fd9db373.png"
+  },
+  {
+    "revision": "93edc0245e727ed784d1997c97d77f09",
+    "url": "./scrum-cards/static/media/planning poker_Squeaking by.93edc024.png"
+  },
+  {
+    "revision": "0b39bb509ebc8dab660694f09e2328d3",
+    "url": "./scrum-cards/static/media/planning poker_When pigs fly.0b39bb50.png"
+  }
+]);
